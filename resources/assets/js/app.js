0f7eb571a3a59483fea3170f07082928cba3f9e6
luindayk <willyanluindayk@gmail.com>
2018-02-18 20:22:27 -0300

    New update

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,18 +7,41 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+// /**
+//  * Next, we will create a fresh Vue application instance and attach it to
+//  * the page. Then, you may begin adding components to this application
+//  * or customize the JavaScript scaffolding to fit your unique needs.
+//  */
 
-Vue.component('example', require('./components/Example.vue'));
+// Vue.component('example', require('./components/Example.vue'));
 
-Vue.component('posts', require('./components/Post.vue'));
+// Vue.component('posts', require('./components/Post.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
+
+import Vue from 'vue';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios);
+
+import App from './components/App.vue';
+import Example from './components/Example.vue';
+
+const routes = [
+  {
+      name: 'Example',
+      path: '/',
+      component: Example
+  }
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
