46aecd4b6f381eb807376dfaed71c6f1a6e725eb
luindayk <willyanluindayk@gmail.com>
2018-02-18 19:05:39 -0300

    Sync project

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,6 +17,8 @@ window.Vue = require('vue');
 
 Vue.component('example', require('./components/Example.vue'));
 
+Vue.component('posts', require('./components/Post.vue'));
+
 const app = new Vue({
     el: '#app'
 });
